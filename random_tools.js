5fac970c5994462b93596f192031a522b44b1502
MuffinTheMan <larsenc91@gmail.com>
2013-01-31 12:10:14 -0600

    Finished date_range function.

--- random_tools.js
+++ random_tools.js
@@ -129,16 +129,32 @@ function date_add(date, months)
 
 function date_range(date1,date2)
 {
-    if(date_diff(date1,date2) < 0)
+    result = new Array();
+    
+    if(date_diff(date1,date2) < 0) // date1 precedes date2
     {
-        
+        result[0] = date1;
+        var tempDate = date_add(date1,1);
+        while(date_diff(tempDate,date2) < 1)
+        {
+            result.push(tempDate);
+            tempDate = date_add(tempDate,1);
+        }
+        return result;
     }
-    else if(date_diff(date1,date2) > 0)
+    else if(date_diff(date1,date2) > 0) // date2 precedes date1
     {
-        
+        result[0] = date2;
+        var tempDate = date_add(date2,1);
+        while(date_diff(tempDate,date1) < 1)
+        {
+            result.push(tempDate);
+            tempDate = date_add(tempDate,1);
+        }
+        return result;
     }
-    else
+    else // The dates are the same (unless the input format is incorrect...may add testing later)
     {
-        return date1; // The dates are the same
+        return date1;
     }
 }
